427ccafbd64647dde735da0076ca3cf5d10901e2
Sandilya-17 <sandilya939174@gmail.com>
2026-05-10 19:00:11 +0530

    Create 0004_issueitem_trip.py

---
+++ backend/apps/inventory/migrations/0004_issueitem_trip.py
@@ -0,0 +1,24 @@
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('inventory', '0003_alter_item_item_type'),
+        ('trips', '0003_trip_fuel_spare_costs'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='issueitem',
+            name='trip',
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                related_name='spare_issues',
+                to='trips.trip',
+            ),
+        ),
+    ]
