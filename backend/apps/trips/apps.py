0168a710333bac6fc16facb897b9476d5cda6da3
Sandilya-17 <sandilya939174@gmail.com>
2026-05-10 20:55:18 +0530

    Update apps.py

--- backend/apps/trips/apps.py
+++ backend/apps/trips/apps.py
@@ -4,3 +4,6 @@ class TripsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.trips'
     label = 'trips'
+
+    def ready(self):
+        import apps.trips.signals  # noqa: F401
