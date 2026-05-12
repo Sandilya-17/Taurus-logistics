855e4ebe9b03b03ff5077328465f756aff223bbb
Sandilya-17 <sandilya939174@gmail.com>
2026-05-12 11:54:43 +0530

    Update signals.py

--- backend/apps/trips/signals.py
+++ backend/apps/trips/signals.py
@@ -1,1 +1,11 @@
 
+"""apps/trips/signals.py"""
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
+
+
+@receiver(pre_delete, sender='trips.Trip')
+def delete_trip_revenue(sender, instance, **kwargs):
+    from apps.finance.models import Revenue
+    Revenue.objects.filter(trip=instance).delete()
+    Revenue.objects.filter(invoice__trip=instance).delete()
