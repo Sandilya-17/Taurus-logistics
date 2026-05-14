32a4416f8346e2fd7d166a8115c07b13f90d7ad6
Sandilya-17 <sandilya939174@gmail.com>
2026-05-14 06:13:22 +0530

    Update 0004_trip_fuel_spare_costs.py

--- backend/apps/trips/migrations/0004_trip_fuel_spare_costs.py
+++ backend/apps/trips/migrations/0004_trip_fuel_spare_costs.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('trips', '0003_trip_fuel_spare_costs'),
+        ('trips', '0002_initial'),
     ]
 
     operations = [
