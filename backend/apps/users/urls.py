e3a6c67b47f941be3506670bebf77936498bd595
Sandilya-17 <sandilya939174@gmail.com>
2026-06-06 10:49:42 +0530

    Update urls.py

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import UserListCreateView, UserDetailView, MeView
 
 urlpatterns = [
-    path('',         UserListCreateView.as_view(), name='user-list'),
-    path('me/',      MeView.as_view(),             name='user-me'),
-    path('<int:pk>/', UserDetailView.as_view(),    name='user-detail'),
+    path('',          UserListCreateView.as_view(), name='user-list'),
+    path('me/',       MeView.as_view(),             name='user-me'),
+    path('<int:pk>/', UserDetailView.as_view(),     name='user-detail'),
 ]
