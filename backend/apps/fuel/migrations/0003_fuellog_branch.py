8f3d292a3cfae7a02044054e505acfeb213745d6
Sandilya-17 <sandilya939174@gmail.com>
2026-06-14 15:58:12 +0530

    Create 0003_fuellog_branch.py

---
+++ backend/apps/fuel/migrations/0003_fuellog_branch.py
@@ -0,0 +1,21 @@
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('fuel', '0002_initial'),
+        ('users', '0003_branch_and_user_role_update'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='fuellog',
+            name='branch',
+            field=models.ForeignKey(
+                blank=True, null=True, db_index=True,
+                on_delete=django.db.models.deletion.PROTECT,
+                related_name='+', to='users.branch',
+            ),
+        ),
+    ]
